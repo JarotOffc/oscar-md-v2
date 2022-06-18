8f2c183532adab0b40cf87380b3874d261d4efad
Rozi <102163283+Botynyakamu@users.noreply.github.com>
2022-06-18 18:20:13 +0700

    Create anime-kanna.js

---
+++ plugins/anime-kanna.js
@@ -0,0 +1,18 @@
+let fetch = require('node-fetch') 
+let handler = async (m, { conn, text, usedPrefix, command }) => { 
+let res = await (await fetch('https://raw.githubusercontent.com/Alfarqun/database/main/anime/kanna.json'))
+ if (!res.ok) throw await `${res.status} ${res.statusText}`; 
+ let json = await res.json(); 
+ let url = json[Math.floor(Math.random() * json.length)] 
+await conn.send2ButtonImg(m.chat, await (await fetch(url)).buffer(), 'Kawai > _<', watermark, 'selanjutnya', `${usedPrefix + command}`, 'owner', '.owner', m)
+}
+
+handler.help = ['kanna']
+handler.tags = ['internet', 'anime'] 
+handler.command = /^(kanna)$/i
+handler.limit = true
+handler.register = true
+
+handler.limit = true
+
+module.exports = handler
