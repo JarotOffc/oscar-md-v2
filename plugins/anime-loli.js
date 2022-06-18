1968f2fed9aef9cd58a9c6eb4e8d341cc93ea88d
Rozi <102163283+Botynyakamu@users.noreply.github.com>
2022-06-18 18:16:04 +0700

    Update anime-loli.js

--- plugins/anime-loli.js
+++ plugins/anime-loli.js
@@ -5,7 +5,7 @@ let res = await fetch(' https://raw.githubusercontent.com/Alfarqun/database/main
 if (!res.ok) throw await `${res.status} ${res.statusText}`;
 let json = await res.json();
 let url = json[Math.floor(Math.random() * json.length)]
-await conn.send2ButtonImg(m.chat, await (await fetch(url)).buffer(), '*nih kak*', watermark, '➡️Selajutnya', '/loli', '🐦owner', '.owner', m)
+await conn.send2ButtonImg(m.chat, await (await fetch(url)).buffer(), '*nih kak*', watermark, 'Selajutnya', '.loli', 'owner', '.owner', m)
 }
 handler.command = /^(loli)$/i
 handler.limit = true
