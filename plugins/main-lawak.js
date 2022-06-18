76e06a8d8e4da44c2c62fff8b928280e0ab4ac69
Rozi <102163283+Botynyakamu@users.noreply.github.com>
2022-06-18 20:29:36 +0700

    Update main-lawak.js

--- plugins/main-lawak.js
+++ plugins/main-lawak.js
@@ -3,7 +3,7 @@ let handler = async (m, { conn, text, usedPrefix, command }) => {
 conn.sendImageAsSticker(m.chat, 'https://telegra.ph/file/3e49a66125d766426ee5f.png', m, { packname: "zifabotz", author: "@rozi" })
 }
 
-handler.customPrefix = /^(wk|wkwk|wkwkwk|wkwkwkwkwk|lucu|ngakak|ngabrut|lawak|lawack)$/i
+handler.customPrefix = /^(wk|wkwk|wkwkwk|wkwkwkwkwk|lucu|ngakak|ngabrut|lawak|lawack|awok|AWOKAWOK|awokawokawok)$/i
 handler.command = new RegExp
 
 module.exports = handler
