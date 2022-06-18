b3f9196cd91a544586bcdc0a635904dfbbb0de42
Rozi <102163283+Botynyakamu@users.noreply.github.com>
2022-06-18 23:02:28 +0700

    Update main-gcbot.js

--- plugins/main-gcbot.js
+++ plugins/main-gcbot.js
@@ -1,8 +1,65 @@
-let handler = async (m, { conn }) => {
-conn.reply(m.chat, gc, m) 
-}
-handler.help = ['gcbot']
-handler.tags = ['main']
-handler.command = /^(gcbot)$/i
+/*============== Jangan di ganti! ==============*/
 
+const { default: makeWASocket, BufferJSON, WA_DEFAULT_EPHEMERAL, generateWAMessageFromContent, downloadContentFromMessage, downloadHistory, proto, getMessage, generateWAMessageContent, prepareWAMessageMedia } = require('@adiwajshing/baileys')
+let fs = require('fs')
+let handler = async (m) => {
+let krizyn = `
+*––––––『 GROUP BOT 』––––––*
+	`.trim()
+  let message = await prepareWAMessageMedia({ image: await (await require('node-fetch')(fotonya4)).buffer()}, { upload: conn.waUploadToServer }) 
+    const template = generateWAMessageFromContent(m.key.remoteJid, proto.Message.fromObject({
+        templateMessage: {
+            hydratedTemplate: {
+                hydratedContentText: krizyn,
+                imageMessage: message.imageMessage, 
+           hydratedFooterText: '```jika mau masuk via link aja kak….```',
+           hydratedButtons: [{
+             urlButton: {
+               displayText: '🐣Via link gc🐣',
+               url: 'https://chat.whatsapp.com/I8Q4oJVw8buHhIgMH5iVAv'
+             }
+
+           },
+                {
+               urlButton: {
+               displayText: '🪀Wa owner🪀',
+               url: 'https://wa.me/message/LSVGQQPG6FUQF1'
+             }
+
+           },
+               {
+             quickReplyButton: {
+               displayText: 'sewa',
+               id: '.sewazifa',
+             }
+             
+           },
+               {
+             quickReplyButton: {
+               displayText: 'Menu',
+               id: '.menu',
+             }
+             
+             },
+               {
+             quickReplyButton: {
+               displayText: 'Donasi',
+               id: '.donasi',
+             }
+
+           }]
+         }
+       }
+     }), { userJid: m.sender, quoted: m });
+    //conn.reply(m.chat, text.trim(), m)
+    return await conn.relayMessage(
+         m.chat,
+         template.message,
+         { messageId: template.key.id }
+     )
+}
+
+handler.tags = ['main', 'info']
+handler.command = /^(gcbot)$/i
+handler.help = ['gcbot']
 module.exports = handler
