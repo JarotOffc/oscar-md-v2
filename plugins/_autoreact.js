044706a3f437e1207b97d7acbd9b62277111cff0
Rozi <102163283+Botynyakamu@users.noreply.github.com>
2022-06-19 09:16:42 +0700

    Update _autoreact.js

--- plugins/_autoreact.js
+++ plugins/_autoreact.js
@@ -2,12 +2,12 @@ let handler = async (m, { conn, text }) => {
 
     conn.sendMessage(m.chat, {
           react: {
-            text: `${pickRandom(['😨','😅','😂','😳','😎', '🥵', '😱', '🐦', '🙄', '🐤','🗿','🐦','🤨','🥴','😐','👆','😔', '👀','👎'])}`,
+            text: `${pickRandom(['😨','😅','😂','😳','😎', '🥵', '😱', '🐦', '🙄', '🐤','🗿','🐈','🤨','🥴','😐','👆','😔', '👀','👎','🧸','🌈'])}`,
             key: m.key,
           }})
   
   }
-handler.customPrefix = /(bot|bang|banh|yes|ya|iya|Rozi|zi|zifa|zifabotz|s|sticker|stiker|kontol|memek|pepek|mmk|ppk|hii|hai|hei|halo|hallo|hello)/i
+handler.customPrefix = /(bot|bang|banh|yes|ya|iya|Rozi|zi|zifa|zifabotz|s|sticker|stiker|kontol|memek|pepek|mmk|ppk|hii|hai|hei|halo|hallo|hello|kak|cum|anjj|anjing)/i
 handler.command = new RegExp
   
 handler.mods = false
