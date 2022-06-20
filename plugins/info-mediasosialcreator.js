c671ef0c9d3b91fac0b7cb9adcbdfc6bc7583e94
Rozi <102163283+Botynyakamu@users.noreply.github.com>
2022-06-20 17:26:52 +0700

    Create info-mediasosialcreator.js

---
+++ plugins/info-mediasosialcreator.js
@@ -0,0 +1,77 @@
+const { default: makeWASocket, BufferJSON, WA_DEFAULT_EPHEMERAL, generateWAMessageFromContent, downloadContentFromMessage, downloadHistory, proto, getMessage, generateWAMessageContent, prepareWAMessageMedia } = require('@adiwajshing/baileys')
+let fs = require('fs')
+let handler = async (m) => {
+let krizyn = `
+Hallo mypren 👋
+Kalian bisa mendukung saya agar bot ini tetap up to date dengan:
+┏━━━〔 ıll *DONATE* llı 〕━━❑
+⬡ *DANA:* ${dana}
+⬡ *GOPAY:* ${gopay}
+⬡ *OVO:* ${pulsa}
+┗━━━━━━━━━━━━━━━━━━❑
+┏━━〔 ıll *THANKS TO* llı 〕━❑
+⬡ team creator-creator
+⬡ team yang sudah berdonasi
+┗━━━━━━━━━━❑
+Berapapun donasi kalian akan sangat berarti 👍
+Terimakasih yang sudah mendonasikan untuk bot
+Contact person Owner:
+wa.me/${numberowner} (Owner)
+	`.trim()
+  let message = await prepareWAMessageMedia({ image: await (await require('node-fetch')(donasi)).buffer()}, { upload: conn.waUploadToServer }) 
+    const template = generateWAMessageFromContent(m.key.remoteJid, proto.Message.fromObject({
+        templateMessage: {
+            hydratedTemplate: {
+                hydratedContentText: krizyn,
+                imageMessage: message.imageMessage, 
+           hydratedFooterText: '📮 Silahkan pilih Command Dibawah ini',
+           hydratedButtons: [{
+             urlButton: {
+               displayText: '📨Tiktok creator📨',
+               url: gc
+             }
+
+           },
+                {
+               urlButton: {
+               displayText: 'Youtube creator🔥',
+               url: nomer
+             }
+
+           },
+               {
+urlButton: {
+               displayText: 'Istagram Creator📸',
+               url: web
+             }
+
+           },
+               {
+             quickReplyButton: {
+               displayText: 'MENU',
+               id: '.sewazifa',
+             }
+             
+           },
+               {
+             quickReplyButton: {
+               displayText: 'SEWA BOT',
+               id: '.sewazifa',
+             }
+
+           }]
+         }
+       }
+     }), { userJid: m.sender, quoted: m });
+    //conn.reply(m.chat, text.trim(), m)
+    return await conn.relayMessage(
+         m.chat,
+         template.message,
+         { messageId: template.key.id }
+     )
+}
+
+handler.tags = ['main', 'info']
+handler.command = /^(mediasosial)$/i
+handler.help = ['mediasosial']
+module.exports = handler
