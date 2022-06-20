f5ec3bedb4c8e5beef8f97730ba7fa3de30b7616
Rozi <102163283+Botynyakamu@users.noreply.github.com>
2022-06-20 18:00:28 +0700

    Create info-biodata.js

---
+++ plugins/info-biodata.js
@@ -0,0 +1,68 @@
+const { default: makeWASocket, BufferJSON, WA_DEFAULT_EPHEMERAL, generateWAMessageFromContent, downloadContentFromMessage, downloadHistory, proto, getMessage, generateWAMessageContent, prepareWAMessageMedia } = require('@adiwajshing/baileys')
+let fs = require('fs')
+let handler = async (m) => {
+let krizyn = `
+❑ *Nama* : Rozi
+❑ *Umur* : 15 Tahun
+❑ *Kelas* : 1 Sma
+❑ *Ulang Tahun* : 4 juli 2007
+❑ *Alamat* : Indonesia, Kalimantan barat, Pontianak
+•·––––––––––––––––––––––––––·•
+	`.trim()
+  let message = await prepareWAMessageMedia({ image: await (await require('node-fetch')(fotonya7)).buffer()}, { upload: conn.waUploadToServer }) 
+    const template = generateWAMessageFromContent(m.key.remoteJid, proto.Message.fromObject({
+        templateMessage: {
+            hydratedTemplate: {
+                hydratedContentText: krizyn,
+                imageMessage: message.imageMessage, 
+           hydratedFooterText: 'masih rozi private bang!',
+           hydratedButtons: [{
+             urlButton: {
+               displayText: 'GroupBot 🐈',
+               url: gc
+             }
+
+           },
+                {
+               urlButton: {
+               displayText: '🌍 Link Istagram 🌍',
+               url: web
+             }
+
+           },
+               {
+             quickReplyButton: {
+               displayText: 'sewa',
+               id: '.sewazifa',
+             }
+             
+           },
+               {
+             quickReplyButton: {
+               displayText: 'Menu',
+               id: '.menu',
+             }
+             
+             },
+               {
+             quickReplyButton: {
+               displayText: 'Donasi bang',
+               id: '.donasi',
+             }
+
+           }]
+         }
+       }
+     }), { userJid: m.sender, quoted: m });
+    //conn.reply(m.chat, text.trim(), m)
+    return await conn.relayMessage(
+         m.chat,
+         template.message,
+         { messageId: template.key.id }
+     )
+}
+
+handler.tags = ['main', 'info']
+handler.command = /^(biodata)$/i
+handler.help = ['biodata']
+module.exports = handler
