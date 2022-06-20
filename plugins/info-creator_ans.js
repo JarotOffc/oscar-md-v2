c6f7065f4c7e9c9087e48d84d74b7e48488ea55f
Rozi <102163283+Botynyakamu@users.noreply.github.com>
2022-06-20 17:44:08 +0700

    Update info-creator_ans.js

--- plugins/info-creator_ans.js
+++ plugins/info-creator_ans.js
@@ -5,7 +5,7 @@ let dana = global.dana
 let pulsa = global.pulsa
 let gopay = global.gopay
 let numberowner = global.numberowner
-let anu = `*TENTANG ROZI:*
+let anu = `*––––––『 INFO ROZI 』––––––*
 Contact person jasa run:
 wa.me/${numberowner} (Owner)
 
