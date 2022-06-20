cbf20bae70a800ff7cb1d36133f3d46aa8442da8
Rozi <102163283+Botynyakamu@users.noreply.github.com>
2022-06-20 17:11:04 +0700

    Create info-creator_ans.js

---
+++ plugins/info-creator_ans.js
@@ -0,0 +1,19 @@
+let fs = require('fs')
+let handler = async (m, { conn }) => {
+let teks = 'Owner'
+let dana = global.dana
+let pulsa = global.pulsa
+let gopay = global.gopay
+let numberowner = global.numberowner
+let anu = `*TENTANG ROZI:*
+Contact person jasa run:
+wa.me/${numberowner} (Owner)
+
+*Follow Instagram ku juga kak😼*`
+  conn.send2ButtonImg(m.chat, fla + teks, anu, instagram, 'Nomor Owner', '.owner2', 'Sosial Media', '.sosialmedia', m) 
+}
+handler.help = ['owner']
+handler.tags = ['info']
+handler.command = /^(owner|creator)$/i
+
+module.exports = handler
