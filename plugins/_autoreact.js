90f7576f81109aff15c2710a8cafe8887e787391
Rozi <102163283+Botynyakamu@users.noreply.github.com>
2022-06-22 13:45:05 +0700

    Update _autoreact.js

--- plugins/_autoreact.js
+++ plugins/_autoreact.js
@@ -10,7 +10,7 @@ let handler = async (m, { conn, text }) => {
 handler.customPrefix = /(bile?k|ban?h|cum?|knt?l|y?|mmk|p|b(a|i)?c?(o|i)?(t|d)?|wibu|p(a)?nt(e)?k|pepe?k|owner)/i
 handler.command = new RegExp
   
-handler.mods = false
+handler.mods = true 
 
 module.exports = handler
 
