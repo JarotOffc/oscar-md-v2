94f44f4bd6ffc9cfff2b43524dbbc538e731bd69
Rozi <102163283+Botynyakamu@users.noreply.github.com>
2022-06-23 12:22:04 +0700

    Update tools-qr.js

--- plugins/tools-qr.js
+++ plugins/tools-qr.js
@@ -1,15 +1,13 @@
 let qrcode = require("qrcode")
-
-let handler = async (m, { conn, text }) => {
-  if (!text) throw 'teksnya mana?'
-  conn.sendFile(m.chat, await qrcode.toDataURL(text.slice(0, 2048), { scale: 8 }), 'qrcode.png', '', m)
+let handler  = async (m, { conn, text }) => {
+  conn.sendFile(m.chat, await qrcode.toDataURL(text.slice(0, 2048), { scale: 8 }), 'qrcode.png', '¯\\_(ツ)_/¯', m)
 }
+
 handler.help = ['', 'code'].map(v => 'qr' + v + ' <teks>')
 handler.tags = ['tools']
 handler.command = /^qr(code)?$/i
-handler.admin = false
-handler.botAdmin = false
 
+handler.limit = true
 handler.fail = null
 
 module.exports = handler
