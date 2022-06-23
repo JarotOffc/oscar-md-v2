f7cc08120d816940e15509b73005b297324ef039
Rozi <102163283+Botynyakamu@users.noreply.github.com>
2022-06-23 12:17:45 +0700

    Update game-tekateki.js

--- plugins/game-tekateki.js
+++ plugins/game-tekateki.js
@@ -1,27 +1,27 @@
 let fetch = require('node-fetch')
-let timeout = 120000
-let poin = 500
 
+let timeout = 180000
+let poin = 500
 let handler = async (m, { conn, usedPrefix }) => {
     conn.tekateki = conn.tekateki ? conn.tekateki : {}
     let id = m.chat
-    if (id in conn.tekateki) return conn.reply(m.chat, 'Belum dijawab!', conn.tekateki[id][0])
-    let res = await fetch(API('amel', '/tekateki', {}, 'apikey'))
-    if (!res.ok) throw eror
-    let json = await res.json()
-    if (!json.status) throw json
-    let caption = `
-${json.soal}
-
+    if (id in conn.tekateki) {
+        conn.reply(m.chat, 'Masih ada soal belum terjawab di chat ini', conn.tekateki[id][0])
+        throw false
+    }
+    let src = await (await fetch('https://raw.githubusercontent.com/qisyana/scrape/main/tekateki.json')).json()
+    let json = src[Math.floor(Math.random() * src.length)]
+    let caption = `${json.pertanyaan}
 Timeout *${(timeout / 1000).toFixed(2)} detik*
-Ketik ${usedPrefix}tete untuk bantuan
+Ketik ${usedPrefix}tekki untuk bantuan
 Bonus: ${poin} XP
+Tiketcoin: 1 Tiketcoin
 `.trim()
     conn.tekateki[id] = [
-        await conn.sendBut(m.chat, caption, wm, 'Bantuan', `.tete`, m),
+        await conn.reply(m.chat, caption, m),
         json, poin,
         setTimeout(() => {
-            if (conn.tekateki[id]) conn.sendBut(m.chat, `Waktu habis!\nJawabannya adalah *${json.jawaban}*`, wm, 'Teka Teki', `.tekateki`, conn.tekateki[id][0])
+            if (conn.tekateki[id]) conn.reply(m.chat, `Waktu habis!\nJawabannya adalah *${json.jawaban}*`, conn.tekateki[id][0])
             delete conn.tekateki[id]
         }, timeout)
     ]
@@ -29,7 +29,7 @@ Bonus: ${poin} XP
 handler.help = ['tekateki']
 handler.tags = ['game']
 handler.command = /^tekateki/i
-
-handler.game = true
+handler.limit = true
+handler.group = true
 
 module.exports = handler
