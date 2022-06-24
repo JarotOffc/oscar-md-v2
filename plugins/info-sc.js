4ba436289e8afe3060c37e09328e14d2a837b393
Rozi <102163283+Botynyakamu@users.noreply.github.com>
2022-06-24 18:10:29 +0700

    Update info-sc.js

--- plugins/info-sc.js
+++ plugins/info-sc.js
@@ -29,7 +29,7 @@ let krizyn = `
            },
                {
              quickReplyButton: {
-               displayText: 'dapatkan session',
+               displayText: 'Dapatkan session',
                id: '.tutorialsession',
              }
              
