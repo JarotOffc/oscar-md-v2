1611485fea75a5c2466678715824a05a1a81f2ee
Rozi <102163283+Botynyakamu@users.noreply.github.com>
2022-06-29 03:36:57 +0700

    Update main-bot.js

--- plugins/main-bot.js
+++ plugins/main-bot.js
@@ -1,9 +1,9 @@
 let handler = async (m, { conn, text, usedPrefix, command }) => {
 
-conn.sendImageAsSticker(m.chat, 'https://telegra.ph/file/5fc650798a93b835ef7e9.png', m, { packname: "zifabotz", author: "@rozi" })
+conn.sendImageAsSticker(m.chat, 'https://telegra.ph/file/70f95bcad271d303bf982.png', m, { packname: "zifabotz", author: "@rozi" })
 }
 
-handler.customPrefix = /^(bot|woi|Cok|ngab|tod|bang|hai)$/i
+handler.customPrefix = /^(bot|woi|Cok|ngab|tod|bang|hai|hi|hii)$/i
 handler.command = new RegExp
 
 module.exports = handler
