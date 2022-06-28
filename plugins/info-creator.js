4fc2dbf94c811b88666c0eb49e87a138210e5fb7
Rozi <102163283+Botynyakamu@users.noreply.github.com>
2022-06-28 12:08:18 +0700

    Update info-creator.js

--- plugins/info-creator.js
+++ plugins/info-creator.js
@@ -1,8 +1,33 @@
-let handler = async(m, { conn }) => {
-conn.sendContact(m.chat, numberowner, nameowner, m)
-}
-handler.help = ['owner2', 'creator2']
+const { default: makeWASocket, BufferJSON, WA_DEFAULT_EPHEMERAL, generateWAMessageFromContent, downloadContentFromMessage, downloadHistory, proto, getMessage, generateWAMessageContent, prepareWAMessageMedia } = require('@adiwajshing/baileys')
+let handler = async (m, {conn}) => {
+const vcard = `BEGIN:VCARD
+VERSION:3.0
+N:;;;
+FN: ᴹᴿ᭄ ZifaOffcོ_1ঔৣ꧂
+item.ORG: ᴹᴿ᭄ ZifaOffcོ_1ঔৣ꧂
+item1.TEL;waid=6285828377610:6285828377610@s.whatsapp.net
+item1.X-ABLabel:owner selalu ada 
+item2.EMAIL;type=INTERNET: anidaanida59@gmail.com
+item2.X-ABLabel:📧Email
+item3.ADR:;;🎍Pontianak;;;;
+item3.X-ABADR:ac
+item3.X-ABLabel:asal kota
+item4.URL:https//github.com/Botynyakamu
+item4.X-ABLabel:Website
+END:VCARD`
+const sentMsg  = await conn.sendMessage(
+    m.chat,
+    { 
+        contacts: { 
+            displayName: 'OWNER BOT', 
+            contacts: [{ vcard }]  
+        }
+    }
+)
+await conn.send3But(m.chat, "*Hai kak👋,  Itu nomor owner gw…*", '📮: jngn di spam kack', 'Source code', '.sc', 'Back', '.menu', 'Donasi', '.donasi', sentMsg)}
+handler.help = ['owner', 'creator']
 handler.tags = ['info']
+
 handler.command = /^(owner2|creator2)$/i
 
 module.exports = handler
