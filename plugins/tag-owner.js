024bbbb6fb6940982c3eddcb330ed94a55eb533f
Rozi <102163283+Botynyakamu@users.noreply.github.com>
2022-06-29 03:28:36 +0700

    Update tag-owner.js

--- plugins/tag-owner.js
+++ plugins/tag-owner.js
@@ -2,7 +2,7 @@ let fs = require('fs')
 let fetch = require('node-fetch')
 let handler = async (m, { conn, text }) => {
 let name = m.fromMe ? conn.user : conn.contacts[m.sender]
-await conn.send3But(m.chat, "*📮: Ada yang bisa saya bantu?*", '', 'SEWA', '.sewa', 'STORE', '.store', 'KEMBALI', '.menu', m)
+await conn.send3But(m.chat, "*=====『 TAG TERDETEKSI 』=====*", '*📮: Ada yang bisa saya bantu?*', 'SEWA', '.sewa', 'STORE', '.store', 'KEMBALI', '.menu', m)
                         
 }
 handler.customPrefix = /@6285828377610|@6285828764046|@6282122977451/i
