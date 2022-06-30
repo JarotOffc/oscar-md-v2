907228ca64fa38892365472f23198c8bb22fa5bf
Rozi88 <108341217+Rozi88@users.noreply.github.com>
2022-06-30 20:11:23 +0700

    Update group-afk.js

--- plugins/group-afk.js
+++ plugins/group-afk.js
@@ -1,19 +1,14 @@
-let handler = (m, { text }) => {
-  let user = global.db.data.users[m.sender]
-  user.afk = + new Date
-  user.afkReason = text
-  let str = `╭──[ *NOW AFK !* ]──✧
+let handler = async(m, { text }) => {
+    let user = global.db.data.users[m.sender]
+    user.afk = +new Date
+    user.afkReason = text
+    m.reply(`╭──[ *TERDETEKSI AFK!* ]──✧
 ┆ *Nama* : ${conn.getName(m.sender)}
 ┆ *Alasan* : ${text ? '' + text : ''}
-╰┅────★
-`.trim()
-conn.sendButton(m.chat, str, wm, 'Jangan Ganggu Ya !', 'iyaaaaa',m)
-conn.reply(str)
+╰┅────`)
 }
-handler.help = ['afk <alasan>']
-handler.tags = ['group']
+handler.help = ['afk [alasan]']
+handler.tags = ['main']
 handler.command = /^afk$/i
 
 module.exports = handler
-
-let wm = global.botwm
