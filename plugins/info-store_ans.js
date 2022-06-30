278ee6bb6ec13e71530c970128b8163ab6aa0349
Rozi88 <108341217+Rozi88@users.noreply.github.com>
2022-06-30 13:04:40 +0700

    Create info-store_ans.js

---
+++ plugins/info-store_ans.js
@@ -0,0 +1,43 @@
+/*============== Jangan di ganti! ==============*/
+
+const { default: makeWASocket, BufferJSON, WA_DEFAULT_EPHEMERAL, generateWAMessageFromContent, downloadContentFromMessage, downloadHistory, proto, getMessage, generateWAMessageContent, prepareWAMessageMedia } = require('@adiwajshing/baileys')
+let fs = require('fs')
+let handler = async (m) => {
+let krizyn = `
+*TUTORIAL BANH!!*
+	`.trim()
+  let message = await prepareWAMessageMedia({ image: await (await require('node-fetch')(fotonya8)).buffer()}, { upload: conn.waUploadToServer }) 
+    const template = generateWAMessageFromContent(m.key.remoteJid, proto.Message.fromObject({
+        templateMessage: {
+            hydratedTemplate: {
+                hydratedContentText: krizyn,
+                imageMessage: message.imageMessage, 
+           hydratedFooterText: '',
+           hydratedButtons: [{
+             urlButton: {
+               displayText: 'Sesssion coding📇',
+               url: ss
+             }
+
+           },
+                {
+               urlButton: {
+               displayText: 'Session simpel🗒',
+               url: sss
+             }
+           }]
+         }
+       }
+     }), { userJid: m.sender, quoted: m });
+    //conn.reply(m.chat, text.trim(), m)
+    return await conn.relayMessage(
+         m.chat,
+         template.message,
+         { messageId: template.key.id }
+     )
+}
+
+handler.tags = ['main', 'info']
+handler.command = /^(tutorialsession)$/i
+handler.help = ['tutorialsesison']
+module.exports = handler
