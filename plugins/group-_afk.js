18f9ebf6b7aa8497761f9ae2fa155d16f57eb3c5
Rozi88 <108341217+Rozi88@users.noreply.github.com>
2022-06-30 20:13:17 +0700

    Update group-_afk.js

--- plugins/group-_afk.js
+++ plugins/group-_afk.js
@@ -1,43 +1,34 @@
 let handler = m => m
 handler.before = m => {
-  let user = global.db.data.users[m.sender]
-  if (user.afk > -1) {
-    let test = `
-╭──[ *BERHENTI AFK !* ]──✧
+    let user = global.db.data.users[m.sender]
+    if (user.afk > -1) {
+        m.reply(`╭──[ *BERHENTI AFK !* ]──✧
 ┆ *Alasan* : ${user.afkReason ? '' + user.afkReason : ''}
 ┆ *Time* : ${clockString(new Date - user.afk)}
-╰┅────★
-`.trim()
-conn.sendButton(m.chat, test, wm, '⋮☰ Menu', '.menu', m)
-conn.reply(test)
-    user.afk = -1
-    user.afkReason = ''
-  }
-  let jids = [...new Set([...(m.mentionedJid || []), ...(m.quoted ? [m.quoted.sender] : [])])]
-  for (let jid of jids) {
-    let user = global.db.data.users[jid]
-    if (!user) continue
-    let afkTime = user.afk
-    if (!afkTime || afkTime < 0) continue
-    let reason = user.afkReason || ''
-    let str = `╭──[ *JANGAN TAG DIA!* ]──✧
+╰┅────★`.trim())
+        user.afk = -1
+        user.afkReason = ''
+    }
+    let jids = [...new Set([...(m.mentionedJid || []), ...(m.quoted ? [m.quoted.sender] : [])])]
+    for (let jid of jids) {
+        let user = global.db.data.users[jid]
+        if (!user) continue
+        let afkTime = user.afk
+        if (!afkTime || afkTime < 0) continue
+        let reason = user.afkReason || ''
+        m.reply(`╭──[ *JANGAN TAG DIA NGAB!* ]──✧
 ┆ ${reason ? '*Alasan* : ' + reason : 'Tanpa Alasan'}
 ┆ *Time* : ${clockString(new Date - afkTime)}
-╰┅────★
-`.trim()
-conn.sendButton(m.chat, str, `${wm}`,'Okiee', 'okeh',m)
-conn.reply(str)
-  }
-  return true
+╰┅────`.trim())
+    }
+    return true
 }
 
 module.exports = handler
 
 function clockString(ms) {
-  let h = isNaN(ms) ? '--' : Math.floor(ms / 3600000)
-  let m = isNaN(ms) ? '--' : Math.floor(ms / 60000) % 60
-  let s = isNaN(ms) ? '--' : Math.floor(ms / 1000) % 60
-  return [h, m, s].map(v => v.toString().padStart(2, 0) ).join(':')
+    let h = isNaN(ms) ? '--' : Math.floor(ms / 3600000)
+    let m = isNaN(ms) ? '--' : Math.floor(ms / 60000) % 60
+    let s = isNaN(ms) ? '--' : Math.floor(ms / 1000) % 60
+    return [h, m, s].map(v => v.toString().padStart(2, 0)).join(':')
 }
-
-let wm = global.wm
