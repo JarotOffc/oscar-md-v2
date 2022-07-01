ccee9f44149fc425f5eb340e3aca756d8fbf6e45
Rozi88 <108341217+Rozi88@users.noreply.github.com>
2022-07-01 23:07:44 +0700

    Update handler.js

--- handler.js
+++ handler.js
@@ -709,7 +709,7 @@ module.exports = {
                         } finally {
                             text = (action === 'add' ? (chat.sWelcome || this.welcome || conn.welcome || 'Yah,si Beban Masuk Grup @user').replace('@subject', groupMetadata.subject).replace('@desc', groupMetadata.desc.toString()) :
                                 (chat.sBye || this.bye || conn.bye || 'Sip, Beban Berkurang @user!')).replace('@user', '@' + user.split('@')[0])
-                                this.send2ButtonImg(id, pp, text, "ᯤ by creator bot", "AWOKAWOAK", "bebanjoin", "MENU", ".menu", null)
+                                this.send2ButtonImg(id, pp, text, "ᯤ by creator bot", "BEBAN", "bebanjoin", "MENU", ".menu", null)
                                 }
                     }
                 }
