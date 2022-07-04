9a2eebc263ca1eb00c8f5ed496108a42bf020064
Rozi88 <108341217+Rozi88@users.noreply.github.com>
2022-07-04 14:10:44 +0700

    Update maker-logololi.js

--- plugins/maker-logololi.js
+++ plugins/maker-logololi.js
@@ -1,13 +1,16 @@
 let fetch = require('node-fetch')
 let handler = async (m, { conn, args }) => {
    response = args.join(' ').split('|')
-  if (!args[0]) throw 'Masukkan Parameter'
-  m.reply('*Wait Ngab*\nProses...')
-  let res = `https://caliphapi.com/api/lolimaker?text=${response[0]}&text2=elyas&apikey=ThiPAkps`
-  conn.sendFile(m.chat, res, 'lolilogo.jpg', `Sudah Jadi`, m, false)
+  if (!args[0]) throw 'Masukkan Teks\nContoh : .logololi zifabotz'
+  m.reply('Proses...')
+  let res = `https://rimurubotz-api.herokuapp.com/api/loli?text=${response[0]}`
+  conn.sendFile(m.chat, res, 'sadboy.jpg', `*Sudah Jadi*`, m, false)
 }
 handler.help = ['logololi'].map(v => v + ' <text>')
-handler.tags = ['maker']
+handler.tags = ['nulis', 'anime']
 handler.command = /^(logololi)$/i
+handler.register = true
+
+handler.limit = true
 
 module.exports = handler
