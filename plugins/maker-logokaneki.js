8142c009fc62beac6cf1ad448d8cdcc4c6627432
Rozi88 <108341217+Rozi88@users.noreply.github.com>
2022-07-04 14:14:11 +0700

    Update maker-logokaneki.js

--- plugins/maker-logokaneki.js
+++ plugins/maker-logokaneki.js
@@ -1,15 +1,16 @@
 let fetch = require('node-fetch')
 let handler = async (m, { conn, args }) => {
-   response = args.join(' ')
-  if (!args[0]) throw 'Masukkan text\nContoh: .logokaneki elyas'
-  m.reply('*Wait ngab*\nProses...')
-  let res = `https://caliphapi.com/api/kaneki?text=${response}&apikey=ThiPAkps`
-  conn.sendFile(m.chat, res, 'kaneki.jpg', `Sudah jadi`, m, false)
+   response = args.join(' ').split('|')
+  if (!args[0]) throw 'Masukkan Teks\nContoh: .logokaneki Zifabotz'
+  m.reply('Proses...')
+  let res = `https://rimurubotz-api.herokuapp.com/api/kaneki?text=${response[0]}`
+  conn.sendFile(m.chat, res, 'kaneki.png', `Sudah Jadi`, m, false)
 }
-handler.help = ['logokaneki'].map(v => v + ' <teks>')
-handler.tags = ['maker']
+handler.help = ['logokaneki'].map(v => v + ' <text>')
+handler.tags = ['nulis', 'anime']
 handler.command = /^(logokaneki)$/i
+handler.register = true
+
 handler.limit = true
-handler.register = false
 
 module.exports = handler
