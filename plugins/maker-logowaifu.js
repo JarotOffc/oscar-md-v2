d7b76c681a9a773529e790199e6a6e5fcb3971f5
Rozi88 <108341217+Rozi88@users.noreply.github.com>
2022-07-04 14:18:12 +0700

    Create maker-logowaifu.js

---
+++ plugins/maker-logowaifu.js
@@ -0,0 +1,16 @@
+let fetch = require('node-fetch')
+let handler = async (m, { conn, args }) => {
+   response = args.join(' ').split('|')
+  if (!args[0]) throw 'Masukkan Teks\nContoh: .logowaifu Zifabotz'
+  m.reply('Proses...')
+  let res = `https://rimurubotz-api.herokuapp.com/api/waifu?text=${response[0]}`
+  conn.sendFile(m.chat, res, 'waifu.png', `*Sudah Jadi*`, m, false)
+}
+handler.help = ['logowaifu'].map(v => v + ' <text>')
+handler.tags = ['nulis', 'anime']
+handler.command = /^(logowaifu)$/i
+handler.register = true
+
+handler.limit = true
+
+module.exports = handler
