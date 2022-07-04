89b7cb7c525954d200088599eb68fcd4179ac3c1
Rozi88 <108341217+Rozi88@users.noreply.github.com>
2022-07-04 14:15:46 +0700

    Update maker-logogura.js

--- plugins/maker-logogura.js
+++ plugins/maker-logogura.js
@@ -1,10 +1,10 @@
 let fetch = require('node-fetch')
 let handler = async (m, { conn, args }) => {
    response = args.join(' ').split('|')
-  if (!args[0]) throw 'Masukkan Parameter'
+  if (!args[0]) throw 'Masukkan Teks\nContoh: .logogura Zifabotz'
   m.reply('Proses...')
   let res = `https://rimurubotz-api.herokuapp.com/api/gura?text=${response[0]}`
-  conn.sendFile(m.chat, res, 'kaneki.png', `Sudah Jadi`, m, false)
+  conn.sendFile(m.chat, res, 'kaneki.png', `*Sudah Jadi*`, m, false)
 }
 handler.help = ['logogura'].map(v => v + ' <text>')
 handler.tags = ['nulis', 'anime']
