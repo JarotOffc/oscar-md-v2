8336866b452bdd9ae75b4e1f6197489410aa903f
Alpiii22 <108053350+Alpiii22@users.noreply.github.com>
2022-08-13 23:57:05 +0700

    OSCAR-MD♨️

--- plugins/maker-logowaifu.js
+++ plugins/maker-logowaifu.js
@@ -1,7 +1,7 @@
 let fetch = require('node-fetch')
 let handler = async (m, { conn, args }) => {
    response = args.join(' ').split('|')
-  if (!args[0]) throw 'Masukkan Teks\nContoh: .logowaifu Zifabotz'
+  if (!args[0]) throw 'Masukkan Teks\nContoh: .logowaifu Oscarbotz'
   m.reply('Proses...')
   let res = `https://rimurubotz-api.herokuapp.com/api/waifu?text=${response[0]}`
   conn.sendFile(m.chat, res, 'waifu.png', `*Sudah Jadi*`, m, false)
