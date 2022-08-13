302067034135e2d8ab158c548b6b230de901eaf9
Alpiii22 <108053350+Alpiii22@users.noreply.github.com>
2022-08-13 21:20:12 +0700

    OSCAR-MD♨️

--- plugins/game-susunkata_hint.js
+++ plugins/game-susunkata_hint.js
@@ -8,5 +8,5 @@ let handler = async (m, { conn }) => {
     conn.reply(m.chat, '```' + clue + '```\nBalas soalnya, bukan pesan ini', conn.susunkata[id][0])
 }
 handler.command = /^suka$/i
-handler.limit = false 
+handler.limit = true
 module.exports = handler
