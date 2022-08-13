42f8f208408222f1238d84814c61a3c27d5faeda
Alpiii22 <108053350+Alpiii22@users.noreply.github.com>
2022-08-13 23:40:25 +0700

    OSCAR-MD♨️

--- plugins/main-nice.js
+++ plugins/main-nice.js
@@ -1,6 +1,6 @@
 let handler = async (m, { conn, text, usedPrefix, command }) => {
 
-conn.sendImageAsSticker(m.chat, 'https://telegra.ph/file/f4ff2dcf458c7b871dd4e.png', m, { packname: "zifabotz", author: "@rozi" })
+conn.sendImageAsSticker(m.chat, 'https://telegra.ph/file/f4ff2dcf458c7b871dd4e.png', m, { packname: "oscarbotz", author: "@jarot" })
 }
 
 handler.customPrefix = /^(nice)$/i
