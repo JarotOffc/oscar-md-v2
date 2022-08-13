3b268b726561d5514f6b51219de9d5fe25a0b58b
Alpiii22 <108053350+Alpiii22@users.noreply.github.com>
2022-08-14 00:05:06 +0700

    OSCAR-MD♨️

--- plugins/nulis-flaming5.js
+++ plugins/nulis-flaming5.js
@@ -1,7 +1,7 @@
 let fetch = require('node-fetch')
 let handler = async (m, { conn, args }) => {
    response = args.join(' ').split('|')
-  if (!args[0]) throw 'Masukkan Text\nContoh : .flaming5 ROZI'
+  if (!args[0]) throw 'Masukkan Text\nContoh : .flaming5 OSCAR'
   m.reply('_Sedang diproses..._')
   let res = `https://api.zeks.me/api/breakwall?apikey=apivinz&text=${response[0]}`
   conn.sendFile(m.chat, res, 'gura.jpg', `*SELESAI. . .*`, m, false)
