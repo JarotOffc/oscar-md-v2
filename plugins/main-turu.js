30e08114e255493ff5ff44fb4f7b665e79f7c0a0
Alpiii22 <108053350+Alpiii22@users.noreply.github.com>
2022-08-13 23:55:02 +0700

    OSCAR-MD♨️

--- plugins/main-turu.js
+++ plugins/main-turu.js
@@ -1,6 +1,6 @@
 let handler = async (m, { conn, text, usedPrefix, command }) => {
 
-conn.sendImageAsSticker(m.chat, 'https://telegra.ph/file/c98503d281e1e251211a0.jpg', m, { packname: "zifabotz", author: "@rozi" })
+conn.sendImageAsSticker(m.chat, 'https://telegra.ph/file/c98503d281e1e251211a0.jpg', m, { packname: "oscarbotz", author: "@jarot" })
 }
 
 handler.customPrefix = /^(turu)$/i
