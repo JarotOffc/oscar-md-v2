47f7eaadb0fabc3f1729c882a147b318d93e2c22
Alpiii22 <108053350+Alpiii22@users.noreply.github.com>
2022-08-13 23:46:47 +0700

    OSCAR-MD♨️

--- plugins/main-sange.js
+++ plugins/main-sange.js
@@ -1,6 +1,6 @@
 let handler = async (m, { conn, text, usedPrefix, command }) => {
 
-conn.sendImageAsSticker(m.chat, 'https://telegra.ph/file/298c47c103f0bca3c1369.png', m, { packname: "zifabotz", author: "@rozi" })
+conn.sendImageAsSticker(m.chat, 'https://telegra.ph/file/298c47c103f0bca3c1369.png', m, { packname: "oscarbotz", author: "@jarot" })
 }
 
 handler.customPrefix = /^(kontol|ngentod|ngentot|memek|pepek|peler)$/i
