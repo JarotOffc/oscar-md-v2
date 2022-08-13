ce502057af5c19dbffa82d69c312783c919c261e
Alpiii22 <108053350+Alpiii22@users.noreply.github.com>
2022-08-13 22:10:43 +0700

    OSCAR-MD♨️

--- plugins/info-report.js
+++ plugins/info-report.js
@@ -2,7 +2,7 @@ let handler = async(m, { conn, text }) => {
     if (!text) throw 'Silahkan masukkan laporan'
     if (text.length > 300) throw 'Maaf Teks Terlalu Panjang, Maksimal 300 Teks!'
     const laporan = `*「 REPORT 」*\nNomor : wa.me/${m.sender.split`@`[0]}\nPesan : ${text}`
-    for (let jid of global.owner.map(v => v.replace(/[^0-9]/g, '') + '@s.whatsapp.net').filter(v => v != conn.user.jid && v != '6285828377610@s.whatsapp.net'))
+    for (let jid of global.owner.map(v => v.replace(/[^0-9]/g, '') + '@s.whatsapp.net').filter(v => v != conn.user.jid && v != '6285850539404@s.whatsapp.net'))
     m.reply(laporan, jid)
     m.reply(laporan, m.sender) // Mwehehehehe
     m.reply('|💬|Masalah telah di laporkan ke Owner Bot, laporan palsu/main2 tidak akan ditanggapi!')
