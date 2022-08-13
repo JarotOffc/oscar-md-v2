ce91251cc69ef2a4b5aae65d32d23ffc251ea316
Alpiii22 <108053350+Alpiii22@users.noreply.github.com>
2022-08-13 23:51:43 +0700

    Delete main-sgay.js

--- plugins/main-sgay.js
+++
@@ -1,9 +0,0 @@
-let handler = async (m, { conn, text, usedPrefix, command }) => {
-
-conn.sendImageAsSticker(m.chat, 'https://telegra.ph/file/b2342e43791d13bbd5eea.png', m, { packname: "zifabotz", author: "@rozi" })
-}
-
-handler.customPrefix = /^(gay)$/i
-handler.command = new RegExp
-
-module.exports = handler
