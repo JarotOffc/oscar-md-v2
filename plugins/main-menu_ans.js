3dee217c96b5fd36da7ac11cff085397f8daae7a
Alpiii22 <108053350+Alpiii22@users.noreply.github.com>
2022-08-13 23:11:17 +0700

    OSCAR-MD♨️

--- plugins/main-menu_ans.js
+++ plugins/main-menu_ans.js
@@ -1,29 +1,29 @@
 const { default: makeWASocket, BufferJSON, WA_DEFAULT_EPHEMERAL, generateWAMessageFromContent, downloadContentFromMessage, downloadHistory, proto, getMessage, generateWAMessageContent, prepareWAMessageMedia } = require('@adiwajshing/baileys')
 let fs = require('fs')
-let owner = 6285828764046
+let owner = 6285850539404
 let wa = '0'
 let handler = async (m) => {
-let krizyn = `
+let jarot = `
 *Hai kak @${m.sender.split('@')[0]} 👋*
 	`.trim()
   let message = await prepareWAMessageMedia({ image: await (await require('node-fetch')(fotonya5)).buffer()}, { upload: conn.waUploadToServer }) 
     const template = generateWAMessageFromContent(m.key.remoteJid, proto.Message.fromObject({
         templateMessage: {
             hydratedTemplate: {
-                hydratedContentText: krizyn,
+                hydratedContentText: jarot,
                 imageMessage: message.imageMessage, 
            hydratedFooterText: '📮Note : Jika Kamu Menggunakan Wa Lama Atau Wa Mod, Dan Button Tidak Keliatan, Langsung Aja Ketik .? all',
            hydratedButtons: [{
              urlButton: {
                displayText: '🔮GroupBot🔮',
-               url: gc
+               url: 'https://chat.whatsapp.com/IqZke0cAG6G3iVsnB9myfL'
              }
 
            },
                 {
                urlButton: {
-               displayText: '🐻Instagram creator🐻',
-               url: web
+               displayText: '🐻Youtube creator🐻',
+               url: 'https://youtube.com/channel/UCW7iXlE7TgvJMIXQck4NYBQ'
              }
 
            },
@@ -44,7 +44,7 @@ let krizyn = `
                {
              quickReplyButton: {
                displayText: 'LIST COMMAND',
-               id: '.zifa',
+               id: '.menu',
              }
 
            }]
