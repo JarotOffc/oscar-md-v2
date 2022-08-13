1cfcd7a5467d664aee6ad0402acb98cafb7212e6
Alpiii22 <108053350+Alpiii22@users.noreply.github.com>
2022-08-14 05:25:20 +0700

    OSCAR-MD♨️

--- plugins/info-sosialmedia.js
+++ plugins/info-sosialmedia.js
@@ -1,7 +1,7 @@
 const { default: makeWASocket, BufferJSON, WA_DEFAULT_EPHEMERAL, generateWAMessageFromContent, downloadContentFromMessage, downloadHistory, proto, getMessage, generateWAMessageContent, prepareWAMessageMedia } = require('@adiwajshing/baileys')
 let fs = require('fs')
 let handler = async (m) => {
-let krizyn = `
+let jarot = `
 ╭─❑ 「 *SOSIAL MEDIA* 」 ❑──
 │ Follow sosial media
 │ owner yuk biar saling
