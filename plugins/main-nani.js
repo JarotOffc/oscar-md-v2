e24e52baae06d1874f27495bcc92ada78991fe0e
Alpiii22 <108053350+Alpiii22@users.noreply.github.com>
2022-08-13 23:38:52 +0700

    OSCAR-MD♨️

--- plugins/main-nani.js
+++ plugins/main-nani.js
@@ -1,6 +1,6 @@
 let handler = async (m, { conn, text, usedPrefix, command }) => {
 
-conn.sendImageAsSticker(m.chat, 'https://telegra.ph/file/133d80c6c852427d7a7ff.png', m, { packname: "zifabotz", author: "@rozi" })
+conn.sendImageAsSticker(m.chat, 'https://telegra.ph/file/133d80c6c852427d7a7ff.png', m, { packname: "oscarbotz", author: "@jarot" })
 }
 
 handler.customPrefix = /^(nani|tes|tess|test|wibu)$/i
