3da0aae7260d459b5b5d240dae6777e5f2eda5b5
Alpiii22 <108053350+Alpiii22@users.noreply.github.com>
2022-08-13 22:37:20 +0700

    OSCAR-MD♨️

--- plugins/main-bot.js
+++ plugins/main-bot.js
@@ -1,6 +1,6 @@
 let handler = async (m, { conn, text, usedPrefix, command }) => {
 
-conn.sendImageAsSticker(m.chat, 'https://telegra.ph/file/70f95bcad271d303bf982.png', m, { packname: "zifabotz", author: "@rozi" })
+conn.sendImageAsSticker(m.chat, 'https://telegra.ph/file/70f95bcad271d303bf982.png', m, { packname: "Oscarbotz", author: "@jarot" })
 }
 
 handler.customPrefix = /^(bot|woi|Cok|ngab|tod|bang|hai|hi|hii)$/i
