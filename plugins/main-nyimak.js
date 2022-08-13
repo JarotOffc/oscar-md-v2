99bd21336ff9b606dee797c2734cee5c78e94e96
Alpiii22 <108053350+Alpiii22@users.noreply.github.com>
2022-08-13 23:42:08 +0700

    OSCAR-MD♨️

--- plugins/main-nyimak.js
+++ plugins/main-nyimak.js
@@ -1,6 +1,6 @@
 let handler = async (m, { conn, text, usedPrefix, command }) => {
 
-conn.sendImageAsSticker(m.chat, 'https://telegra.ph/file/b9907b917e10db107e464.png', m, { packname: "zifabotz", author: "@rozi" })
+conn.sendImageAsSticker(m.chat, 'https://telegra.ph/file/b9907b917e10db107e464.png', m, { packname: "oscarbotz", author: "@jarot" })
 }
 
 handler.customPrefix = /^(nyimak|menyimak)$/i
