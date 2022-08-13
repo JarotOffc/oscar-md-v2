ee83cb9a24544524b25b22a1526baf488a8ad8e1
Alpiii22 <108053350+Alpiii22@users.noreply.github.com>
2022-08-13 23:37:11 +0700

    OSCAR-MD♨️

--- plugins/main-nangis.js
+++ plugins/main-nangis.js
@@ -1,6 +1,6 @@
 let handler = async (m, { conn, text, usedPrefix, command }) => {
 
-conn.sendImageAsSticker(m.chat, 'https://telegra.ph/file/44c08e07a1c81411d5a69.jpg', m, { packname: "zifabotz", author: "@rozi" })
+conn.sendImageAsSticker(m.chat, 'https://telegra.ph/file/44c08e07a1c81411d5a69.jpg', m, { packname: "oscarbotz", author: "@jarot" })
 }
 
 handler.customPrefix = /^(😭|😭😭|😭😭😭|😭😭😭😭|😭😭😭😭😭|😭😭😭😭😭😭)$/i
