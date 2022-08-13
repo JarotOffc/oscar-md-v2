f6404a4f99ce9e35cdb638ffe1605c7b148ed1cd
Alpiii22 <108053350+Alpiii22@users.noreply.github.com>
2022-08-13 21:37:35 +0700

    OSCAR-MD♨️

--- plugins/group-afk.js
+++ plugins/group-afk.js
@@ -2,7 +2,7 @@ let handler = async(m, { text }) => {
     let user = global.db.data.users[m.sender]
     user.afk = +new Date
     user.afkReason = text
-    m.reply(`╭──[ *TERDETEKSI AFK!* ]──✧
+    m.reply(`╭──[ *TERDETEKSI AFK!!* ]──✧
 ┆ *Nama* : ${conn.getName(m.sender)}
 ┆ *Alasan* : ${text ? '' + text : ''}
 ╰┅────❑`)
