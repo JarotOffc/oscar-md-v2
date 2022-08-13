0e40aa31470b6c3417e25643134fa8858757613d
Alpiii22 <108053350+Alpiii22@users.noreply.github.com>
2022-08-14 00:08:38 +0700

    OSCAR-MD♨️

--- plugins/owner-bcgc.js
+++ plugins/owner-bcgc.js
@@ -7,7 +7,7 @@ let handler = async (m, { conn,isOwner, isROwner, text }) => {
     if(!pesan) throw 'teksnya?'
     m.reply(`Mengirim Broadcast Ke ${anu.length} Chat, Waktu Selesai ${anu.length * 0.5} detik`)
     for (let i of anu) {
-    conn.send3ButtonImg(i, 'https://telegra.ph/file/de2dcb0a83bb606d3c72a.jpg', pesan, wm, 'Menu', '.menu', 'Owner', '.owner', 'Donasi', '.donasi')
+    conn.send3ButtonImg(i, 'https://telegra.ph/file/b6b25d5ff74febec06fb4.jpg',pesan, wm, 'Menu', '.menu', 'Owner', '.owner', 'Donasi', '.donasi')
     }
   m.reply(`Sukses Mengirim Broadcast Ke ${anu.length} Group`)
 }
