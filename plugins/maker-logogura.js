d1e58e09a033de8f500489e522824ec564ead4ea
Alpiii22 <108053350+Alpiii22@users.noreply.github.com>
2022-08-13 23:56:01 +0700

    OSCAR-MD♨️

--- plugins/maker-logogura.js
+++ plugins/maker-logogura.js
@@ -1,7 +1,7 @@
 let fetch = require('node-fetch')
 let handler = async (m, { conn, args }) => {
    response = args.join(' ').split('|')
-  if (!args[0]) throw 'Masukkan Teks\nContoh: .logogura Zifabotz'
+  if (!args[0]) throw 'Masukkan Teks\nContoh: .logogura Oscarbotz'
   m.reply('Proses...')
   let res = `https://rimurubotz-api.herokuapp.com/api/gura?text=${response[0]}`
   conn.sendFile(m.chat, res, 'kaneki.png', `*Sudah Jadi*`, m, false)
