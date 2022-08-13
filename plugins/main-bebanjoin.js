92a65e70d6231db7a4b6c36999a322874996fb4d
Alpiii22 <108053350+Alpiii22@users.noreply.github.com>
2022-08-13 22:34:04 +0700

    OSCAR-MD♨️

--- plugins/main-bebanjoin.js
+++ plugins/main-bebanjoin.js
@@ -1,6 +1,6 @@
 let handler = async (m, { conn, text, usedPrefix, command }) => {
 
-conn.sendImageAsSticker(m.chat, 'https://telegra.ph/file/2f08d4a54fa18d67d04d6.png', m, { packname: "zifabotz", author: "@rozi" })
+conn.sendImageAsSticker(m.chat, 'https://telegra.ph/file/2f08d4a54fa18d67d04d6.png', m, { packname: "Oscarbotz", author: "@jarot" })
 }
 
 handler.customPrefix = /^(bebanjoin)$/i
