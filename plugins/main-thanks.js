4225d9b2497c5c9396e222b8da29f1e8c3741347
Alpiii22 <108053350+Alpiii22@users.noreply.github.com>
2022-08-13 23:53:16 +0700

    OSCAR-MD♨️

--- plugins/main-thanks.js
+++ plugins/main-thanks.js
@@ -14,7 +14,7 @@ krizyn = {
         }
        } 
      }
-conn.sendImageAsSticker(m.chat, 'https://telegra.ph/file/06a27a93b66f63a5a067e.jpg', krizyn, { packname: "Krizyn Ofc", author: "@mhdfakri_" })
+conn.sendImageAsSticker(m.chat, 'https://telegra.ph/file/06a27a93b66f63a5a067e.jpg', krizyn, { packname: "Oscar Ofc", author: "@mhdjarot_" })
 }
 
 handler.customPrefix = /^(Terima Kasih|Thanks|tq|makasih)$/i
