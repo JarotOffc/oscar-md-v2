183d067fd555b2b736ef6e8245b613079d7cf775
Alpiii22 <108053350+Alpiii22@users.noreply.github.com>
2022-08-13 23:45:40 +0700

    OSCAR-MD♨️

--- plugins/main-p.js
+++ plugins/main-p.js
@@ -1,6 +1,6 @@
 let handler = async (m, { conn, text, usedPrefix, command }) => {
 
-conn.sendImageAsSticker(m.chat, 'https://telegra.ph/file/db38dcd2fcbe07ec66348.jpg', m, { packname: "zifabotz", author: "@rozi" })
+conn.sendImageAsSticker(m.chat, 'https://telegra.ph/file/db38dcd2fcbe07ec66348.jpg', m, { packname: "oscarbotz", author: "@jarot" })
 }
 
 handler.customPrefix = /^(p)$/i
