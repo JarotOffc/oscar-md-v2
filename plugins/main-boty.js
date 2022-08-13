6f0a4913b5ea5e415008aebaa6764f4f12b44ead
Alpiii22 <108053350+Alpiii22@users.noreply.github.com>
2022-08-13 22:39:14 +0700

    OSCAR-MD♨️

--- plugins/main-boty.js
+++ plugins/main-boty.js
@@ -1,6 +1,6 @@
 let handler = async (m, { conn, text, usedPrefix, command }) => {
 
-conn.sendImageAsSticker(m.chat, 'https://telegra.ph/file/9bc8b84760a433b4d26a3.jpg', m, { packname: "zifabotz", author: "@rozi" })
+conn.sendImageAsSticker(m.chat, 'https://telegra.ph/file/9bc8b84760a433b4d26a3.jpg', m, { packname: "Oscarbotz", author: "@Jarot" })
 }
 
 handler.customPrefix = /^(boti|boty|boty binal)$/i
