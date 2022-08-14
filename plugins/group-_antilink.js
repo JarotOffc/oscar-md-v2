0bdaceb5dd1233ec40ff3b1166d5a5ca6d9044df
kyyyyy27 <107806866+kyyyyy27@users.noreply.github.com>
2022-08-15 00:37:27 +0700

    Update group-_antilink.js

--- plugins/group-_antilink.js
+++ plugins/group-_antilink.js
@@ -1,20 +1,24 @@
 let handler = m => m
 
 let linkRegex = /chat.whatsapp.com\/([0-9A-Za-z]{20,24})/i
+handler.before = async function (m, { user, isBotAdmin, isAdmin }) {
+  if ((m.isBaileys && m.fromMe) || m.fromMe || !m.isGroup) return true
+  let chat = global.db.data.chats[m.chat]
+  let isGroupLink = linkRegex.exec(m.text)
 
-handler.before = function (m, { user, isAdmin, isBotAdmin }) {
-
-  if (m.isBaileys && m.fromMe) throw false
-  let chat = global.DATABASE.data.chats[m.chat]
-  let name = this.getName(m.sender)
-  let link = linkRegex.exec(m.text)
-
-  if (chat.antiLink && link && !isAdmin && !m.isBaileys && m.isGroup && !m.fromMe) {
-  if (user.isAdmin || user.isSuperAdmin) return m.reply ('Karena Kamu Adalah Admin Group Bot tidak akan Kick Kamu')
- m.reply(`*「 ANTILINK TERDETEKSI 」*\nLink Gorup Terdeteksi Maaf *${name}* Kamu Telah Mengirim Link Group Kamu Akan Saya Kick Lain Kali Kalo Share Cek Deskripsi Ya Deck:(`)
-   this.groupParticipantsUpdate(m.chat, [m.sender], 'remove')
+  if (chat.antiLink && isGroupLink) {
+    await m.reply(`*「 ANTI LINK 」*\n\nDetected *${await conn.getName(m.sender)}* you have sent the group link!\n\nSorry you will be kicked from this group byee!`)
+    if (isAdmin) return m.reply('*wah anjir , situ admin gabisa dikik 😢. hehe..*')
+    if (!isBotAdmin) return m.reply('*Bot nya bukan admin, mana bisa ngekik 😐*')
+    let linkGC = ('https://chat.whatsapp.com/' + await conn.groupInviteCode(m.chat))
+    let isLinkconnGc = new RegExp(linkGC, 'i')
+    let isgclink = isLinkconnGc.test(m.text)
+    if (isgclink) return m.reply('*「 ANTI LINK 」*\n\nItu Dilarang❗, Maaf kamu akan saya kik.\nKarena kirim Link👍')
+    await conn.groupParticipantsUpdate(m.chat, [m.sender], "remove")
   }
+  return true
 }
-handler.group = true
 
 module.exports = handler
+
+
