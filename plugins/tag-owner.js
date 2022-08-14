dfd434696373965e86cc11ef31b1a34e9e34c323
kyyyyy27 <107806866+kyyyyy27@users.noreply.github.com>
2022-08-15 00:25:48 +0700

    Update tag-owner.js

--- plugins/tag-owner.js
+++ plugins/tag-owner.js
@@ -2,10 +2,10 @@ let fs = require('fs')
 let fetch = require('node-fetch')
 let handler = async (m, { conn, text }) => {
 let name = m.fromMe ? conn.user : conn.contacts[m.sender]
-let jarot = 'https://telegra.ph/file/966adedcd3b40f50fa794.jpg'
+let jarot = 'https://telegra.ph/file/a871f2ca2c623c57b82dd.jpg'
 await conn.send2ButtonImg(m.chat, jarot, "*BIMLEK🐦*", 'ngapain banh tag” dia', 'OWNER', '.owner', 'KEMBALI', '.menu', m)                        
 }
-handler.customPrefix = /@6285850539404,79609055704/i
+handler.customPrefix = /@6283830815715,6283830815715/i
 handler.command = new RegExp
 
 module.exports = handler
